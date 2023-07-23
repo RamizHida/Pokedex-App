6e2d5747226c8d56ad84bb1b4af746338a00db6e
Ramiz Hida <ramizhida00@gmail.com>
2023-07-23 02:10:41 +0200

    Usearray  method to populate Pokemon array

--- js/script.js
+++ js/script.js
@@ -1,27 +1,36 @@
-let pokemonList = [
-  {
-    name: 'Squirtle',
-    height: '5',
-    types: ['water'],
-  },
-  {
-    name: 'Charizard',
-    height: '17',
-    types: ['fire', 'flying'],
-  },
-  {
-    name: 'Jigglypuff',
-    height: '5',
-    types: ['fairy', 'normal'],
-  },
-  {
-    name: 'Dugtrio',
-    height: '7',
-    types: ['ground'],
-  },
-  {
-    name: 'Weepinbell',
-    height: '1',
-    types: ['grass', 'poision'],
-  },
-];
+// Empty Array that will store the pokemon data
+let pokemonList = [];
+
+// Pokemon objects
+let squirtle = {
+  name: 'Squirtle',
+  height: '5',
+  types: ['water'],
+};
+
+let charizard = {
+  name: 'Charizard',
+  height: '17',
+  types: ['fire', 'flying'],
+};
+
+let jigglyPuff = {
+  name: 'Jigglypuff',
+  height: '5',
+  types: ['fairy', 'normal'],
+};
+
+let dugTrio = {
+  name: 'Dugtrio',
+  height: '7',
+  types: ['ground'],
+};
+
+let weepinBell = {
+  name: 'Weepinbell',
+  height: '1',
+  types: ['grass', 'poision'],
+};
+
+// Add individual pokemon objects to a single pokemon array
+pokemonList.push(squirtle, charizard, jigglyPuff, dugTrio, weepinBell);
