936e60b8920442fcacf10ef66c79ef6aa67ad1b6
Ramiz Hida <ramizhida00@gmail.com>
2023-07-25 12:59:02 +0200

    Display pokemon details on DOM

--- js/script.js
+++ js/script.js
@@ -4,25 +4,25 @@ let pokemonList = [];
 // Pokemon objects
 let squirtle = {
   name: 'Squirtle',
-  height: '5',
+  height: '0.5',
   types: ['water'],
 };
 
 let charizard = {
   name: 'Charizard',
-  height: '17',
+  height: '1.7',
   types: ['fire', 'flying'],
 };
 
 let jigglyPuff = {
   name: 'Jigglypuff',
-  height: '5',
+  height: '0.5',
   types: ['fairy', 'normal'],
 };
 
 let dugTrio = {
   name: 'Dugtrio',
-  height: '7',
+  height: '0.7',
   types: ['ground'],
 };
 
@@ -34,3 +34,16 @@ let weepinBell = {
 
 // Add individual pokemon objects to a single pokemon array
 pokemonList.push(squirtle, charizard, jigglyPuff, dugTrio, weepinBell);
+
+// Display pokemon name and height to the DOM
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height < 1.5) {
+    document.write(
+      `${pokemonList[i].name} (height: ${pokemonList[i].height}) <br> <br>`
+    );
+  } else {
+    document.write(
+      `${pokemonList[i].name} (height: ${pokemonList[i].height}) :  Wow, that's big! <br><br>`
+    );
+  }
+}
