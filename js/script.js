062506521eb64fc91579bce41443629d1e7f516a
Ramiz Hida <ramizhida00@gmail.com>
2023-08-02 16:38:28 +0200

    Implement modal and made pokemon container responsive

--- js/script.js
+++ js/script.js
@@ -21,6 +21,7 @@ let pokemonRepository = (function () {
     let btn = document.createElement('button');
     btn.innerText = 'Show Details';
     btn.classList.add('btn');
+    btn.classList.add('hover');
     btn.addEventListener('click', function () {
       showDetails(pokemon);
     });
@@ -31,7 +32,7 @@ let pokemonRepository = (function () {
 
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
-      console.log(pokemon);
+      modal.showModal(pokemon);
     });
   }
 
@@ -89,3 +90,76 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+let modal = (function () {
+  let pokeMonContainer = document.querySelector('.pokemon-container');
+  let modalContainer = document.querySelector('.modal-container');
+  let closeBtn = document.createElement('button');
+
+  function showModal(pokemon) {
+    // Prevent opening modal if modal is already opened
+    if (modalContainer.getAttribute('class').includes('is-visible')) return;
+
+    // Fade background
+    pokeMonContainer.style.opacity = 0.5;
+
+    let name = pokemon.name;
+    let height = pokemon.height;
+    let imgURL = pokemon.imageUrl;
+
+    modalContainer.classList.add('is-visible');
+
+    let pokemonName = document.createElement('h1');
+    pokemonName.innerText = name;
+
+    closeBtn.setAttribute('id', 'close-modal-btn');
+    closeBtn.innerText = 'x';
+
+    let pokemonFeatures = document.createElement('div');
+    pokemonFeatures.setAttribute('id', 'pokemon-features');
+    let pokemonHeight = document.createElement('p');
+    pokemonHeight.innerText = 'height: ' + height;
+    let pokemonImg = document.createElement('img');
+    pokemonImg.src = imgURL;
+
+    pokemonFeatures.appendChild(pokemonHeight);
+    pokemonFeatures.appendChild(pokemonImg);
+
+    modalContainer.appendChild(pokemonName);
+    modalContainer.appendChild(pokemonFeatures);
+    modalContainer.appendChild(closeBtn);
+  }
+
+  function hideModal() {
+    // Empty modal container
+    modalContainer.innerHTML = '';
+
+    // Undo fade of background
+    pokeMonContainer.style.opacity = 1;
+
+    // Remove modal from screen
+    modalContainer.classList.remove('is-visible');
+  }
+
+  // Close modal via pressing the 'x' button
+  closeBtn.addEventListener('click', hideModal);
+
+  // Close modal via pressing 'esc' key
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  // Close modal via clicking outside of modal and pokemonContainer
+  window.addEventListener('click', (e) => {
+    if (e.target === pokeMonContainer) {
+      hideModal();
+    }
+  });
+
+  return {
+    showModal: showModal,
+    hideModal: hideModal,
+  };
+})();
